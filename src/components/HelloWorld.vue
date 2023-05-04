8641073bf47aed8e32709767dd79c561e2955c1e
darshanwohlig19 <darshan.gada@wohlig.com>
2023-05-04 13:09:46 +0530

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,367 @@
+<template>
+  <div class="first">
+    <div class="second">
+      <v-row class="third">
+        <v-btn class="fourth" style="background-color: black"
+          ><h5>esc</h5></v-btn
+        >
+        <v-btn class="fourth" style="background-color: black"
+          ><h5>F1</h5></v-btn
+        >
+        <v-btn class="fourth" style="background-color: black"
+          ><h5>F2</h5></v-btn
+        >
+        <v-btn class="fourth" style="background-color: black"
+          ><h5>F3</h5></v-btn
+        >
+        <v-btn class="fourth" style="background-color: black"
+          ><h5>F4</h5></v-btn
+        >
+        <v-btn class="fourth" style="background-color: black"
+          ><h5>F5</h5></v-btn
+        >
+        <v-btn class="fourth" style="background-color: black"
+          ><h5>F6</h5></v-btn
+        >
+        <v-btn class="fourth" style="background-color: black"
+          ><h5>F7</h5></v-btn
+        >
+        <v-btn class="fourth" style="background-color: black"
+          ><h5>F8</h5></v-btn
+        >
+        <v-btn class="fourth" style="background-color: black"
+          ><h5>F9</h5></v-btn
+        >
+        <v-btn class="fourth" style="background-color: black"
+          ><h5>F10</h5></v-btn
+        >
+        <v-btn class="fourth" style="background-color: black"
+          ><h5>F11</h5></v-btn
+        >
+        <v-btn class="fourth" style="background-color: black"
+          ><h5>F12</h5></v-btn
+        >
+        <v-btn class="fourth" style="background-color: black"
+          ><h5>power</h5></v-btn
+        >
+      </v-row>
+
+      <v-row style="margin-top:2%" class="third">
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>~</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>1</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>2</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>3</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>4</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>5</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>6</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>7</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>8</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>9</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>0</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>-</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>+</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>delete</h5></v-btn
+        >
+      </v-row>
+
+      <v-row style="margin-top:2%" class="third">
+        <v-btn
+          class="fourth"
+          height="50px"
+          width="83px"
+          style="background-color: black"
+          ><h5>tab</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>Q</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>W</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>E</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>R</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>T</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>Y</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>U</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>I</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>O</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>P</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>[</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>]</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>\</h5></v-btn
+        >
+      </v-row>
+
+      <v-row style="margin-top:2%" class="third">
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>caps lock</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>A</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>S</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>D</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>F</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>G</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>H</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>J</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>K</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>L</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>:</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>'</h5></v-btn
+        >
+        <v-btn
+          class="fourth"
+          height="50px"
+          width="115px"
+          style="background-color: black"
+          ><h5>enter</h5></v-btn
+        >
+      </v-row>
+
+      <v-row style="margin-top:2%" class="third">
+        <v-btn
+          class="fourth"
+          height="50px"
+          width="148px"
+          style="background-color: black"
+          ><h5>shift</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>Z</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>X</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>C</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>V</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>B</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>N</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>M</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>,</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>.</h5></v-btn
+        >
+        <v-btn class="fourth" height="50px" style="background-color: black"
+          ><h5>/</h5></v-btn
+        >
+        <v-btn
+          class="fourth"
+          height="50px"
+          width="148px"
+          style="background-color: black"
+          ><h5>shift</h5></v-btn
+        >
+      </v-row>
+
+      <v-row style="margin-top:2%" class="third">
+        <v-btn class="fourth" height="60px" style="background-color: black"
+          ><h5>fn</h5></v-btn
+        >
+        <v-btn
+          class="fourth"
+          height="60px"
+          width="20px"
+          style="background-color: black"
+          ><h5>control</h5></v-btn
+        >
+        <v-btn
+          class="fourth"
+          height="60px"
+          width="20px"
+          style="background-color: black"
+          ><h5>option</h5></v-btn
+        >
+        <v-btn
+          class="fourth"
+          height="60px"
+          width="80px"
+          style="background-color: black"
+          ><h5>command</h5></v-btn
+        >
+        <v-btn
+          class="fourth"
+          height="60px"
+          width="340px"
+          style="background-color: black"
+          ><h5>spacebar</h5></v-btn
+        >
+        <v-btn
+          class="fourth"
+          height="60px"
+          width="80px"
+          style="background-color: black"
+          ><h5>command</h5></v-btn
+        >
+        <v-btn
+          class="fourth"
+          height="60px"
+          width="20px"
+          style="background-color: black"
+          ><h5>option</h5></v-btn
+        >
+        <v-layout style="margin-top:1%" class="car">
+          <v-row class="vehicle">
+            <v-btn
+              class="fourth"
+              height="30px"
+              width="72px"
+              style="background-color: black"
+              ><h5>up</h5></v-btn
+            >
+          </v-row>
+          <v-row class="bus">
+            <v-btn class="fourth" height="30px" style="background-color: black"
+              ><h5>left</h5></v-btn
+            >
+
+            <v-btn class="fourth" height="30px" style="background-color: black"
+              ><h5>down</h5></v-btn
+            >
+            <v-btn class="fourth" height="30px" style="background-color: black"
+              ><h5>right</h5></v-btn
+            >
+          </v-row>
+        </v-layout>
+      </v-row>
+      <v-row class="d-flex justify-center">
+        <v-col cols="12" md="4" class="d-flex justify-center">
+          <div class="fifth"></div>
+        </v-col>
+      </v-row>
+    </div>
+  </div>
+</template>
+<style scoped>
+.first {
+  height: 720px;
+  width: auto;
+  background-color: #aaa9ad;
+  border: 3px solid black;
+  border-radius: 2%;
+}
+.second {
+  margin: auto;
+  margin-top: 20px;
+  height: 375px;
+  width: 1100px;
+  border: 3px solid black;
+  border-radius: 4%;
+}
+.third {
+  margin-top: 10px;
+  margin-left: 15px;
+}
+.fourth {
+  margin-left: 10px;
+}
+h5 {
+  color: white;
+}
+.fifth {
+  margin-top: 40px;
+  /* margin-left: 30%; */
+  height: 250px;
+  width: 400px;
+  background-color: #aaa9ad;
+  border: 4px solid white;
+  border-radius: 5%;
+}
+.car {
+  height: 65px;
+  width: 55px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  /* background-color: blue; */
+}
+
+.vehicle {
+  margin-left: 83px;
+}
+
+.bus {
+  margin-top: -2%;
+  margin-left: 8px;
+}
+</style>
